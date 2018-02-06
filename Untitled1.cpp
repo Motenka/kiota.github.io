b36b4bf766288cb60476bff37c6348db6136f617
Motenka <36182136+Motenka@users.noreply.github.com>
2018-02-06 10:54:43 +0400

    demo

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while (1)
+        {
+            cout << i << " ";
+        i = i + 1;
+    if(i > 100)
+        break; // прерывание цикла
+        }
+return 0;
+}
